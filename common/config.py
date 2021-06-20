bc31b58ca6161ad9b2732780ed020f9728d4ed85
nenad-misic <33166172+nenad-misic@users.noreply.github.com>
2021-06-20 16:08:30 +0200

    Model rework (#26)
    * Add rabbitmq consumer component
    
    * Rework model to be flask independent
    
    * Rework model to be flask independent closes #25

--- common/config.py
+++ common/config.py
@@ -1,46 +1,18 @@
 from os import environ
-from typing import Tuple
 from flask_cors import CORS
 from flask.app import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from flask_restful import Api
-from common.database import db
-from models.models import Block, Comment, Favorite, Follow, Like, Post, Tagged, User
-from controller.post_controller import PostResource, PostListResource, ProfileResource, LikeResource, DislikeResource, FavoriteResource, CommentResource
 
-
-DevConfig = {
-    'DEBUG': True,
-    'DEVELOPMENT': True,
-    'SQLALCHEMY_DATABASE_URI': f'{environ.get("DB_TYPE")}+{environ.get("DB_DRIVER")}://{environ.get("DB_USER")}:{environ.get("DB_PASSWORD")}@{environ.get("DB_HOST")}/{environ.get("DB_NAME")}',
-    'SQLALCHEMY_ECHO': True,
-    'SQLALCHEMY_TRACK_MODIFICATIONS': False
+config = {
+    'test': 'TEST_DATABASE_URI',
+    'dev': 'DEV_DATABASE_URI'
 }
 
-
-ProdConfig = {}
-
-
-TestConfig = {
-    'DEBUG': False,
-    'DEVELOPMENT': True,
-    'SQLALCHEMY_DATABASE_URI': f'{environ.get("TEST_DATABASE_URI")}',
-    'SQLALCHEMY_ECHO': True,
-    'SQLALCHEMY_TRACK_MODIFICATIONS': False
-}
-
-
-config: dict = {
-    'dev': DevConfig,
-    'prod': ProdConfig,
-    'test': TestConfig
-}
-
-
-def setup_config(cfg_name: str) -> Tuple[Flask, SQLAlchemy]:
+def setup_config(cfg_name: str):
+    environ['SQLALCHEMY_DATABASE_URI'] = environ.get(config[cfg_name])
+    
     app = Flask(__name__)
-
     if environ.get('ENABLE_CSRF') == 1:
         app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
         app.config['WTF_CSRF_SECRET_KEY'] = environ.get('WTF_CSRF_SECRET_KEY')
@@ -48,10 +20,17 @@ def setup_config(cfg_name: str) -> Tuple[Flask, SQLAlchemy]:
         csrf.init_app(app)
         
     CORS(app, resources={r"/*": {"origins": "http://localhost:3000", "send_wildcard": "False"}})
-
     api = Api(app)
 
+
+    # This import must be postponed because importing common.database has side-effects
+    from common.database import init_db
+    init_db()
+
+
     
+    # This import must be postponed after init_db has been called
+    from controller.post_controller import PostResource, PostListResource, ProfileResource, LikeResource, DislikeResource, FavoriteResource, CommentResource
     api.add_resource(PostResource, '/api/<post_id>')
     api.add_resource(PostListResource, '/api')
     api.add_resource(ProfileResource, '/api/profile/<user_id>')
@@ -60,16 +39,9 @@ def setup_config(cfg_name: str) -> Tuple[Flask, SQLAlchemy]:
     api.add_resource(FavoriteResource, '/api/favorite/<post_id>')
     api.add_resource(CommentResource, '/api/comment/<post_id>')
 
-    cfg = config.get(cfg_name)
-    for key in cfg.keys():
-        app.config[key] = cfg[key]
 
-    app.app_context().push()
-    db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
-
+    # This import must be postponed after init_db has been called
+    from models.models import Block, Comment, Favorite, Follow, Like, Post, Tagged, User
     if cfg_name == 'test':
         Block.query.delete()
         Comment.query.delete()
@@ -80,4 +52,4 @@ def setup_config(cfg_name: str) -> Tuple[Flask, SQLAlchemy]:
         Post.query.delete()
         User.query.delete()
 
-    return app, db
+    return app
