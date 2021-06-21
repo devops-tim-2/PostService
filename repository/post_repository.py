2ce86adb2ce7e7d90192700084436900789edc13
Miloš Radojčin <milos.radojcin.97@gmail.com>
2021-06-21 21:12:44 +0200

    Feature/get users posts (#30)
    * Create post
    
    * Tests for creating new post
    
    * Get post
    
    * Refactor table name for User and fix integration tests close #7
    
    * Get user's posts close #8
    
    * Fix tests

--- repository/post_repository.py
+++ repository/post_repository.py
@@ -8,6 +8,9 @@ def create(post: Post):
 def get(post_id: int) -> Post:
     return Post.query.get(post_id)
 
+def get_users_posts(user_id: int):
+    return Post.query.filter_by(user_id=user_id).all()
+
 def delete_by_id(id):
     Post.query.filter_by(id=id).delete()
     db_session.commit()
