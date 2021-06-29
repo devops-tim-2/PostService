1001ca21d61a68eb9657e727330d229f32e82f4a
Miloš Radojčin <milos.radojcin.97@gmail.com>
2021-06-29 12:43:44 +0200

    Implement like and dislike

--- service/like_service.py
+++ service/like_service.py
@@ -1,4 +1,44 @@
+from models.models import Like
 from repository import like_repository
+from service import post_service
 
 def delete_with_post(post_id: int):
     like_repository.delete_with_post(post_id)
+
+def like(post_id: int, user: dict):
+    post_service.get(post_id, user)
+
+    like = like_repository.get(post_id, user['id'])
+    if not like:
+        like = Like(post_id=post_id, user_id=user['id'], state=True)
+    
+        like_repository.save(like)
+    else:
+        state = like.state
+
+        like_repository.delete(like.id)
+        if not state:
+            like = Like(post_id=post_id, user_id=user['id'], state=True)
+        
+            like_repository.save(like)
+
+    return True
+
+def dislike(post_id: int, user: dict):
+    post_service.get(post_id, user)
+
+    like = like_repository.get(post_id, user['id'])
+    if not like:
+        like = Like(post_id=post_id, user_id=user['id'], state=False)
+    
+        like_repository.save(like)
+    else:
+        state = like.state
+
+        like_repository.delete(like.id)
+        if state:
+            like = Like(post_id=post_id, user_id=user['id'], state=False)
+        
+            like_repository.save(like)
+
+    return True
